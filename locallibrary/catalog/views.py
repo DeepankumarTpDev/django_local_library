5df1c7b8eed4f2f1c0a119df4c8217fd1d30adba
Deepankumar <deepankumar@testpress.in>
2024-08-30 07:26:06 +0000

    Add ListView and DetailView

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Book,BookInstance,Author,Genre
+from django.views import generic
 # Create your views here.
+
+
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
@@ -19,3 +22,11 @@ def index(request):
         'num_BooksContains_Murder' : num_BooksContains_Murder
     }
     return render(request, 'index.html', context = context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+class BookDetailView(generic.DetailView):
+    model = Book
